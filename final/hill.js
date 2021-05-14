2a160d20f8c78b3328666b48c94efda56a373a2f
kakasoo <kscodebase@gmail.com>
2021-05-14 21:14:53 +0900

    인터렉티브 프로그래밍을 참고하여 언덕과 태양 애니메이션 제작

---
+++ final/hill.js
@@ -0,0 +1,83 @@
+export class Hill {
+    constructor(color, speed, total) {
+        this.color = color;
+        this.speed = speed;
+        this.total = total;
+    }
+
+    resize(stageWidth, stageHeight) {
+        this.stageWidth = stageWidth;
+        this.stageHeight = stageHeight;
+
+        this.points = [];
+        this.gap = Math.ceil(this.stageWidth / (this.total - 2)); // stage보다 크게 생성
+
+        for (let i = 0; i < this.total; i++) {
+            this.points[i] = {
+                x: i * this.gap,
+                y: this.getY(),
+            };
+        }
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+
+        let cur = this.points[0];
+        let prev = cur;
+
+        let dots = [];
+        cur.x += this.speed;
+
+        // 언덕이 소멸하지 않도록 배열의 맨 앞에 새로운 점을 계속 추가해간다.
+        if (cur.x > -this.gap) {
+            this.points.unshift({
+                x: -(this.gap * 2),
+                y: this.getY(),
+            });
+        } else if (cur.x > this.stageWidth + this.gap) {
+            this.points.splice(-1);
+        }
+
+        ctx.moveTo(cur.x, cur.y);
+
+        let prevCx = cur.x;
+        let prevCy = cur.y;
+
+        for (let i = 1; i < this.points.length; i++) {
+            cur = this.points[i];
+            cur.x += this.speed;
+
+            const cx = (prev.x + cur.x) / 2;
+            const cy = (prev.y + cur.y) / 2;
+            ctx.quadraticCurveTo(prev.x, prev.y, cx, cy);
+
+            dots.push({
+                x1: prevCx,
+                y1: prevCy,
+                x2: prev.x,
+                y2: prev.y,
+                x3: cx,
+                y3: cy,
+            });
+
+            prev = cur;
+            prevCx = cx;
+            prevCy = cy;
+        }
+
+        ctx.lineTo(prev.x, prev.y); // 마지막 점
+        ctx.lineTo(this.stageWidth, this.stageHeight); // 우측 맨 아래
+        ctx.lineTo(this.points[0].x, this.stageHeight); // 첫번째 점의 x 기준 맨 아래
+        ctx.fill();
+
+        return dots;
+    }
+
+    getY() {
+        const min = this.stageHeight / 8;
+        const max = this.stageHeight - min;
+        return min + Math.random() * max;
+    }
+}
